e6266982605222af440d9cdb84656df622d3cdc5
Kawan Carvalho <kawanvinicius14@gmail.com>
2020-03-10 22:01:16 -0300

    Projeto aula 1

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using ExemploAula01.Negocio;
+using ExemploAula01.Repositorio;
+using System;
+
+namespace ExemploAula01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var _repo = new LivroRepositorioCSV();
+
+            ImprimeLista(_repo.ParaLer);
+            ImprimeLista(_repo.Lendo);
+            ImprimeLista(_repo.Lidos);
+
+            Console.ReadLine();
+        }
+
+        static void ImprimeLista(ListaDeLeitura lista)
+        {
+            Console.WriteLine(lista);
+        }
+    }
+}
---
+++ Repositorio/ILivroRepositorio.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ExemploAula01.Negocio;
+
+namespace ExemploAula01.Repositorio
+{
+    interface ILivroRepositorio
+    {
+        ListaDeLeitura ParaLer { get; }
+
+        ListaDeLeitura Lendo { get; }
+
+        ListaDeLeitura Lidos { get; }
+
+        IEnumerable<Livro> Todos { get; }
+
+        void Incluir(Livro livro);
+    }
+}
